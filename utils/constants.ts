2e463a564c9af7f6bb4619df4f2cb8aefa8520fa
cohsh <82075327+cohsh@users.noreply.github.com>
2024-05-25 17:04:00 +0900

    :zap: updated sample pages

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,2 +1,9 @@
 export const TEST_CHAR = "\\Psi";
 export const TEST_EQ = "f(x) = \\int_{-\\infty}^\\infty dy g(x-y)";
+
+// Mathematical symbols
+export const DEFINE = ":=";
+
+// Numbers
+export const NATURAL_NUMBER = "\\mathbb{N}"
+export const INTEGER = "\\mathbb{Z}"
