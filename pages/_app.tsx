9203343ab01c9888d6e8c0aed2957a539ca42320
cohsh <82075327+cohsh@users.noreply.github.com>
2024-05-25 12:32:33 +0900

    :zap: added pages/_app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import { AppProps } from "next/app";
+import "katex/dist/katex.min.css";
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+    <Component {...pageProps} />
+};
+
+export default MyApp;
