e6ac8dc061e4d8c8cfb4bc3a7355c1a257f4db80
cohsh <82075327+cohsh@users.noreply.github.com>
2024-05-25 12:40:47 +0900

    :zap: removed pages/_app.tsx for error

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import { AppProps } from "next/app";
-import "katex/dist/katex.min.css";
-
-const MyApp = ({ Component, pageProps }: AppProps) => {
-    <Component {...pageProps} />
-};
-
-export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
 
+import { InlineMath, BlockMath } from "react-katex";
+import "katex/dist/katex.min.css";
+
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Formulae Base</h1>
