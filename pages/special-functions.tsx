3cbb3a0085be65910ca864fc0ab640aee53562ef
cohsh <82075327+cohsh@users.noreply.github.com>
2024-05-25 16:09:45 +0900

    :zap: updated format

--- pages/special-functions.tsx
+++ pages/special-functions.tsx
@@ -5,20 +5,35 @@ import "react-toastify/dist/ReactToastify.css";
 
 const IndexPage = () => {
     const formulas = [
-        { title: "Gamma Function", formula: `\\Gamma(z) = \\int_0^\\infty dt t^{z-1}e^{-t},~\\mathrm{Re}(z) > 0` },
-        { title: "Beta Function", formula: `B(z, z')=\\int_0^1 dt t^{z-1}(1-t)^{z'-1},~\\mathrm{Re}(z),\\mathrm{Re}(z')>0` },
+        {
+            title: "Gamma Function",
+            definitions: [
+                { description: "Definition", formula: `\\Gamma(z) = \\int_0^\\infty dt t^{z-1}e^{-t},~\\mathrm{Re}(z) > 0` },
+                { description: "Alternative Definition", formula: `\\Gamma(n) = (n-1)!` }
+            ]
+        },
+        {
+            title: "Beta Function",
+            definitions: [
+                { description: "Beta Function Definition", formula: `B(z, z')=\\int_0^1 dt t^{z-1}(1-t)^{z'-1},~\\mathrm{Re}(z),\\mathrm{Re}(z')>0` },
+                { description: "Relation with Gamma Function", formula: `B(z, z') = \\frac{\\Gamma(z)\\Gamma(z')}{\\Gamma(z+z')}` }
+            ]
+        },
     ];
     
     return (
         <Layout title="Home | Next.js + TypeScript Example">
             <h2>Special Functions</h2>
-            {formulas.map(({ title, formula }) => (
-            <div key={title}>
-                <h3>{title}</h3>
-                <CopyableMathBlock formula={formula} />
-            </div>
+            {formulas.map(({ title, definitions }) => (
+                <div key={title}>
+                    <h3>{title}</h3>
+                    {definitions.map(({ formula }, index) => (
+                        <div key={index}>
+                            <CopyableMathBlock formula={formula} />
+                        </div>
+                    ))}
+                </div>
             ))}
-
             <ToastContainer />
         </Layout>
     )
