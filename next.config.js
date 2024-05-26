ad616eaff291b3ec7b5dbc260854cd90631c08ec
cohsh <82075327+cohsh@users.noreply.github.com>
2024-05-26 10:21:27 +0900

    :zap: updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
     output: 'export',
+
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
     assetPrefix: process.env.NODE_ENV === 'production' ? '/formulae-base/' : '',
     basePath: process.env.NODE_ENV === 'production' ? '/formulae-base' : '',
     images: {
         unoptimized: true,
     },
-};
+}
+
+module.exports = nextConfig
